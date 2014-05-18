5dd33d2a2ad05953fa472d5ebf405ed451c654fd
Jare Fagbemi <ofagbemi@stanford.edu>
2014-05-17 18:12:57 -0700

    Fix to last

--- db/migrate/20140518010310_change_politics_as_sport_image.rb
+++ db/migrate/20140518010310_change_politics_as_sport_image.rb
@@ -1,6 +1,9 @@
 class ChangePoliticsAsSportImage < ActiveRecord::Migration
   def change
     a = Article.find_by_title("Politics as Sport")
+    
     a.images[0].url = "http://www.dixiesunnews.com/media/news/2012/10/23/photo/PresidentialDebatesMCT.jpg"
+    
+    a.update_attribute :images, a.images
   end
 end
